3f607c531ab6d1b2076fb7b33284158735985a15
ThanhCuong <adidasgttn@gmail.com>
2023-04-13 23:17:58 +0700

    bt

--- Day 05-Biến và kiểu dữ liệu/script.js
+++ Day 05-Biến và kiểu dữ liệu/script.js
@@ -1,2 +1,6 @@
 // confirm("Bạn có muốn xóa sản phẩm không")
 // console.log("xin chao");
+const PI =3.14;
+let R = prompt("Nhập bán kính đường tròn")
+let c = 2*R*PI;
+console.log("Chu vi đường tròn là: "+c);
