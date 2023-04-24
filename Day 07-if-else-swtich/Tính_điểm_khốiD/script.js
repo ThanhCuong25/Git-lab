e5954335956cd18494f37dd1c6badc44c914fd17
ThanhCuong <adidasgttn@gmail.com>
2023-04-24 23:05:14 +0700

    điểm

--- Day 07-if-else-swtich/Tính_điểm_khốiD/script.js
+++ Day 07-if-else-swtich/Tính_điểm_khốiD/script.js
@@ -1,25 +1,35 @@
 let toanElement = document.getElementById('toan');
 let vanElement = document.getElementById('van');
 let anhElement = document.getElementById('anh');
-let lcElement = document.getElementById('lc');
-let kv1Element = document.getElementById('kv1');
-let kv2Element = document.getElementById('kv2');
-let kvNT2Element = document.getElementById('kvNT2');
-let kv3Element = document.getElementById('kv3');
+let areaElement = document.getElementById('area');
 let tinhDiemElement = document.getElementById('tinhDiem');
 let tongDiemElement = document.getElementById('tongDiem');
-
-tinhDiemElement.addEventListener('click',function() {
-    console.log('sfdf');
+const KV1 = "kv1";
+const KV2 = "kv2";
+const KV2_NT = "kv2-NT";
+const KV3 = "kv3";
+tinhDiemElement.addEventListener('click', function () {
     let toan = toanElement.value;
     let van = vanElement.value;
     let anh = anhElement.value;
-    let lc = lcElement.value;
-    
-    switch(lc){
-        case kv1Element:
-            let total = +toan + +van + +anh +0.75;
-            tongDiemElement.innerHTML =`Tổng điểm ĐH khối D của bạn là: ${total}`;
+    let area = areaElement.value;
+    let areaPoint
+    switch (area) {
+        case KV1:
+           areaPoint = 0.75
+            break;
+        case KV2:
+            areaPoint= 0.25
+            break;
+        case KV2_NT:
+            areaPoint=0.5
+            break;
+        case KV3:
+            areaPoint = 0;      
+            break;
     }
+
+           let total = +toan + +van + +anh + Number(areaPoint);
+            tongDiemElement.innerHTML = `Tổng điểm ĐH khối D của bạn là: ${total}`;
 
 })
